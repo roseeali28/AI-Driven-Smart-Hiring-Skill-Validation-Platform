debf76b664489a5bd546a9411e01b5cbd5224fd3
YUV RAJ SINGH YADAV <yuvrajyadav730@gmail.com>
2026-04-03 22:57:19 +0530

    Fix formatting of required property in AptitudeResult

--- live-code/livecode/models/AptitudeResult.js
+++ live-code/livecode/models/AptitudeResult.js
@@ -8,7 +8,7 @@ const aptitudeResultSchema = new mongoose.Schema({
     },
     topic: {
         type: String,
-        required: true
+        required: true  
     },
     category: {
         type: String,
